9841c56ad437890db54560cfb5774184aefa82bd
lr75021 <lr75021@gmail.com>
2017-03-17 15:30:29 -0700

    initial update

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # awsdemo3
 demo 3 for AWS EC2 instance (node.js, express)
+
+Project outline:
+- run node.js
+- use express framework
+- use json data file
+- use angular.js for graph
+
--- test3.js
+++ test3.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-	res.send('Hello again!\n');
+	res.send('Hello world - test3 - ver1.0!\n');
 });
 
 var port = 9000;
